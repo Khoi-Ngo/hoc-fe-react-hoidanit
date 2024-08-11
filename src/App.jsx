201b0bf88a182fb459c7937663e6e4a8a137f7fa
Khoi Ngo <khoingo2003@gmail.com>
2024-08-11 21:40:47 +0700

    prepare for todo list application

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from './components/learn/MyComponent'
-import { SecondComponent, AdditionalSecondComponent } from './components/learn/MySecondComponent'
-
-
 
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const hello = () => {
-    console.log(`Arrow function in an arrow function`);
-  }
-  hello();
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Test change something in the code - Hello world</h1>
-      <MyComponent />
-      <SecondComponent />
-      <AdditionalSecondComponent />
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{ textAlign: "center" }}>test hello world</div>
     </>
   )
 }
