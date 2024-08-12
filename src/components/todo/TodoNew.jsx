7ace939f7486cca7aa98b3622ef6f6b9ae7ceff3
Khoi Ngo <khoingo2003@gmail.com>
2024-08-12 21:02:15 +0700

    passing function with param in event html dom onchange + onclick

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,29 @@
 const TodoNew = (props) => {
-    const {addNewTodo} = props;
-    addNewTodo(`created by Khoi Ngo`);
+    const { addNewTodo } = props;
+    // addNewTodo(`created by Khoi Ngo`);
+    const handleClickAddBtn = () => {
+        alert(`You sure about that`);
+    }
+
+    //! onchange event in react inputted param event having many usages
+
+    const someFunction = (event) => {
+        console.log(`Check inputted text: ${event.target.value}`);
+    }
+
+
+    const handleOnchangeInput = (someText) => {
+        console.log(`Check inputted text: ${someText}`);
+    }
     return (
         <>
             <div className='todo-input-form'>
-                <input type="text" placeholder='Type something todo' />
-                <button className='btn btn-success'>ADD</button>
+                <input type="text" placeholder='Type something todo'
+                    onChange={(event) => { handleOnchangeInput(event.target.value) }}
+                />
+                <button className='btn btn-success' style={{ cursor: "pointer" }}
+                    onClick={handleClickAddBtn}
+                >ADD</button>
             </div>
         </>
     );
