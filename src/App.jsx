688cccb735aa95cc33d4344ec963d40a2e20feed
Khoi Ngo <khoingo2003@gmail.com>
2024-08-12 19:19:20 +0700

    inherting properties components

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 
+
 const App = () => {
+
+  const testInheritVar1 = `Khoi Ngo1`;
+  const testInheritVar2 = `Khoi Ngo2`;
+  const testInheritVar3 = `Khoi Ngo3`;
+  const testInheritVar4 = `Khoi Ngo4`;
+  const testInheritVar5 = `Khoi Ngo5`;
+  const someOtherData = { address: `tphcm`, country: `vietnam` };
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">TODO list app - ReactJS</div>
         <TodoNew />
-        <TodoData />
+        <TodoData
+          name={[testInheritVar1, testInheritVar2, testInheritVar3, testInheritVar4, testInheritVar5]}
+          someOtherData={someOtherData}
+        />
         <div className='todo-image'>
           <img src={reactLogo} className='logo' />
         </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,15 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    console.log(`>>>> Check props: ${Object.values(props)}`);
+    console.log(props);
+
+
+    const {name, someOtherData} = props;
+    
+
     return (<>
         <div className='task-list-table'>
+            <div>My name is {someOtherData.address}</div>
+            <div>{JSON.stringify(name)}</div>
             <div>Learning React</div>
             <div>Coding Java</div>
         </div>
