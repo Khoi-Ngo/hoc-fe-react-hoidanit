ac22d0b500efdb3bc54b11ce009e8213dd2e3240
Khoi Ngo <khoingo2003@gmail.com>
2024-08-13 23:00:32 +0700

    defining some page using react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/error-components/errorpage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/login",
+    element: <div> login page</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/register",
+    element: <div> register page</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/users",
+    element: <div> users page</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/products",
+    element: <div> products page</div>,
+    errorElement: <ErrorPage />
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
