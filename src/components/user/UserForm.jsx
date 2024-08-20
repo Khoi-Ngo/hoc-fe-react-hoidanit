f24eb06ceedb481ef6b90bdce4c238205a19734d
Khoi Ngo <khoingo2003@gmail.com>
2024-08-20 23:31:49 +0700

    Design Modal Create User

--- src/components/user/UserForm.jsx
+++ src/components/user/UserForm.jsx
@@ -1,7 +1,8 @@
-import { Button, Input, message, notification } from "antd";
+import { Button, Input, message, Modal, notification } from "antd";
 import './UserForm.css';
 import { useState } from "react";
 import { createUserAPI } from "../../service/api_service";
+import { ExclamationCircleFilled } from "@ant-design/icons";
 
 
 
@@ -13,8 +14,16 @@ const UserForm = () => {
     const [email, setEmail] = useState(``);
     const [password, setPassword] = useState(``);
     const [phone, setPhone] = useState(``);
+    const [isModalOpen, setIsModelOpen] = useState(false);
 
-    const handleOnClick = async () => {
+    const handleOnClick = () => {
+        setIsModelOpen(true);
+    }
+
+
+
+
+    const handleOnClickOKCreateModal = async () => {
         let response = await createUserAPI(fullName, email, password, phone);
         // console.log(`Check response promise from axios: ${response.data.data}`);
         if (response.data != null) {//after going through interceptor the response 
@@ -24,51 +33,95 @@ const UserForm = () => {
                     description: "Tạo mới user thành công"
                 }
             );
-        }else{
+        } else {
             notification.error({
                 message: "Cannot create user",
                 description: JSON.stringify(response.message)
             })
         }
+        //close modal
+        setIsModelOpen(false);
+        //reload page
+        setTimeout(() => {
+            window.location.reload();
+        }, 2500);
+    }
+
+
+    const handleCancelClickModalCreate = () => {
+        Modal.confirm({
+            title: 'Confirm',
+            icon: <ExclamationCircleFilled />,
+            okText: 'Yes, Cancel',
+            cancelText: 'No, Continue',
+            onOk: () => {
+                setIsModelOpen(false);
+                notification.info({
+                    message: 'Action Cancelled',
+                    description: 'The form has been closed.',
+                });
+            },
+            onCancel: () => {
+                notification.info({
+                    message: 'Continue Editing',
+                    description: 'You can continue filling out the form.',
+                });
+            },
+        });
+        // setIsModelOpen(false);
     }
 
     return (
         <>
             <div className="user-form">
-                <div>
-                    <span>Full name</span>
-                    <Input placeholder="Input full name"
-                        onChange={(event) => {
-                            setFullName(event.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input placeholder="Input email"
-                        onChange={(event) => {
-                            setEmail(event.target.value);
-                        }} />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password placeholder="Input password"
-                        onChange={(event) => {
-                            setPassword(event.target.value);
-                        }} />
-                </div>
-                <div>
-                    <span>Phone number</span>
-                    <Input placeholder="Phone number"
-                        onChange={(event) => {
-                            setPhone(event.target.value);
-                        }} />
-                </div>
+
                 <div>
                     <Button type='primary'
                         onClick={handleOnClick}
                     >Create User</Button>
                 </div>
+
+                <Modal title="Create User Form"
+                    open={isModalOpen}
+                    onOk={handleOnClickOKCreateModal}
+                    onCancel={handleCancelClickModalCreate}
+                    maskClosable={false}
+                    okText={"Yes, Create this user !"}
+                    cancelText={"Just kidding, no need create!"}
+                >
+                    <div>
+                        <span>Full name</span>
+                        <Input placeholder="Input full name"
+                            onChange={(event) => {
+                                setFullName(event.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <span>Email</span>
+                        <Input placeholder="Input email"
+                            onChange={(event) => {
+                                setEmail(event.target.value);
+                            }} />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <Input.Password placeholder="Input password"
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                            }} />
+                    </div>
+                    <div>
+                        <span>Phone number</span>
+                        <Input placeholder="Phone number"
+                            onChange={(event) => {
+                                setPhone(event.target.value);
+                            }} />
+                    </div>
+                </Modal>
+
+
+
             </div>
         </>
 
@@ -76,3 +129,5 @@ const UserForm = () => {
 }
 
 export default UserForm;
+
+
